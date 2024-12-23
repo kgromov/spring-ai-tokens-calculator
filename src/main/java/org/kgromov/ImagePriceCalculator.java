8bc805edca57025f261fe94d4fcb9c5a6ad8c59a
konstantin <konst.gromov@gmail.com>
2024-12-24 01:11:11 +0200

    Define next steps to implement image model calculator

--- src/main/java/org/kgromov/ImagePriceCalculator.java
+++ src/main/java/org/kgromov/ImagePriceCalculator.java
@@ -2,6 +2,13 @@ package org.kgromov;
 
 import org.springframework.ai.chat.model.ChatResponse;
 
+/*
+ * TODO: implementation:
+ * it seems to be missed advisor for image request
+ * so as an attempt to implement - try out aspect around call to ImageModel#call(ImagePrompt request);
+ * from ImagePrompt it' possible to fetch resolution (alternatively can be done from Image response - ImageResponse#getResult().getOutput())
+ * result images can be just fetched from ImageResponse#getResults().size()
+ */
 public final class ImagePriceCalculator implements PriceCalculator {
     private final ImageModelProperties properties;
 
--- src/main/java/org/kgromov/PriceCalculator.java
+++ src/main/java/org/kgromov/PriceCalculator.java
@@ -4,5 +4,6 @@ import org.springframework.ai.chat.model.ChatResponse;
 
 public sealed interface PriceCalculator permits AudioPriceCalculator, EmbeddingsPriceCalculator, ImagePriceCalculator, TextModelPriceCalculator {
 
+    // TODO: change to ModelResponse or generify ModelResponse as interface generic type
     double calculate(ChatResponse response);
 }
